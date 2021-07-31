acbe0be656c6e3cd46acbf51a9748fe316716089
Templars <antoine.ratat@gmail.com>
2021-07-31 18:48:58 +0800

    navbar link css

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
 			<Container fluid className='container_home'>
 				<Navbar className='navbar' />
 				<div id='overlay'></div>
-				{/* <video
+				<video
 					id='background_big'
 					src='https://res.cloudinary.com/della-vite/video/upload/f_auto,q_auto,ac_none,f_auto,w_1400/dato/1598299900-dvwebsitehomepagegeneral16x9.mp4'
 					loop='loop'
@@ -27,7 +27,7 @@ export default function Home() {
 					autoPlay='autoplay'
 					playsInline=''
 					className='backgroundMedia'
-					data-video='0'></video> */}
+					data-video='0'></video>
 				<Row style={{ backgroundColor: 'yellow', border: '1px solid black' }}>
 					<Col style={{ backgroundColor: 'grey', border: '1px solid black' }}>Home</Col>
 				</Row>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,26 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { Navbar as BootstrapNavbar, Nav } from 'react-bootstrap'
 
+function onMouseEnter(e) {
+	let navbar_nav = document.getElementsByClassName('navbar-nav')[0]
+	let navbar_child = navbar_nav.children
+	for (var i = 0; i < navbar_child.length; i++) {
+		let navbar_link = navbar_child[i]
+		if (navbar_link.id !== e.target.getAttribute('id')) {
+			navbar_link.style.color = '#ffffff47'
+		}
+	}
+}
+
+function onMouseExit(e) {
+	let navbar_nav = document.getElementsByClassName('navbar-nav')[0]
+	let navbar_child = navbar_nav.children
+	for (var i = 0; i < navbar_child.length; i++) {
+		let navbar_link = navbar_child[i]
+		navbar_link.style.color = 'white'
+	}
+}
+
 export default function Navbar() {
 	return (
 		<>
@@ -12,16 +32,44 @@ export default function Navbar() {
 				<BootstrapNavbar.Toggle aria-controls='basic-navbar-nav' />
 				<BootstrapNavbar.Collapse id='basic-navbar-nav' className='navbar_nav'>
 					<Nav className='mr-auto'>
-						<NavLink exact className='navbar_wines' activeClassName='current' to='/wines'>
+						<NavLink
+							exact
+							id='navbar_wines'
+							className='navbar_wines'
+							activeClassName='current'
+							to='/wines'
+							onMouseEnter={(e) => onMouseEnter(e)}
+							onMouseLeave={(e) => onMouseExit(e)}>
 							Wines
 						</NavLink>
-						<NavLink exact className='navbar_story' activeClassName='current' to='/story'>
+						<NavLink
+							exact
+							id='navbar_story'
+							className='navbar_story'
+							activeClassName='current'
+							to='/story'
+							onMouseEnter={(e) => onMouseEnter(e)}
+							onMouseLeave={(e) => onMouseExit(e)}>
 							Story
 						</NavLink>
-						<NavLink exact className='navbar_winery' activeClassName='current' to='/winery'>
+						<NavLink
+							exact
+							id='navbar_winery'
+							className='navbar_winery'
+							activeClassName='current'
+							to='/winery'
+							onMouseEnter={(e) => onMouseEnter(e)}
+							onMouseLeave={(e) => onMouseExit(e)}>
 							Winery
 						</NavLink>
-						<NavLink exact className='navbar_contact' activeClassName='current' to='/contact'>
+						<NavLink
+							exact
+							id='navbar_contact'
+							className='navbar_contact'
+							activeClassName='current'
+							to='/contact'
+							onMouseEnter={(e) => onMouseEnter(e)}
+							onMouseLeave={(e) => onMouseExit(e)}>
 							Contact
 						</NavLink>
 					</Nav>
