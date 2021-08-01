bfa889b8a7412ad7c533295c3185f166425931d5
Templars <antoine.ratat@gmail.com>
2021-08-01 14:33:58 +0800

    video global

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ import { routes } from './routes'
 function App() {
 	return (
 		<BrowserRouter>
+			<video
+				id='background_big'
+				src='https://res.cloudinary.com/della-vite/video/upload/f_auto,q_auto,ac_none,f_auto,w_1400/dato/1598299900-dvwebsitehomepagegeneral16x9.mp4'
+				loop='loop'
+				muted='muted'
+				autoPlay='autoplay'
+				playsInline=''
+				className='backgroundMedia'
+				data-video='0'></video>
+
+			<video
+				id='background_small'
+				src='https://res.cloudinary.com/della-vite/video/upload/f_auto,q_auto,ac_none,f_auto,w_900/dato/1598299917-dvwebsitehomepagegeneral9x16.mp4'
+				loop='loop'
+				muted='muted'
+				autoPlay='autoplay'
+				playsInline=''
+				className='backgroundMedia'
+				data-video='0'></video>
 			<Switch>
 				{getRoutes(routes)}
 				<Redirect from='/' to='/home' />
