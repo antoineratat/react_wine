df59dd52c9257b4787bbf21149bdaca57be7fc1c
Templars <antoine.ratat@gmail.com>
2021-08-03 19:51:13 +0800

    contact left

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import { Container, Row, Col } from 'react-bootstrap'
+import { Animated } from 'react-animated-css'
 
 export default function Contact() {
 	return (
@@ -9,11 +10,33 @@ export default function Contact() {
 			<Container fluid className='container_home'>
 				<Navbar className='navbar' />
 				<div id='overlay'></div>
-				<Row style={{ backgroundColor: 'yellow', border: '1px solid black' }}>
-					<Col style={{ backgroundColor: 'grey', border: '1px solid black' }}>Contact</Col>
-				</Row>
-				<Footer />
+				<Container fluid className='container_contact'>
+					<Animated animationIn='fadeIn' animationOut='fadeOut' animationInDuration={1000} isVisible={true}>
+						<Container fluid className='container_inner_contact'>
+							<Row>
+								<Col className='contact_left'>
+									<Row>
+										<h1>Company Name</h1>
+									</Row>
+									<Row>
+										<h4>
+											Rich and extra-dry with a highly mineral palate that lingers, our Prosecco Superiore DOCG is characterised by hints
+											of crostini and chamomile.
+										</h4>
+									</Row>
+									<Row>
+										<p>45.9013° N, 11.9959° E, NORTHERN ITALY</p>
+									</Row>
+								</Col>
+							</Row>
+							<Row>
+								<Col className='contact_right'></Col>
+							</Row>
+						</Container>
+					</Animated>
+				</Container>
 			</Container>
+			<Footer />
 		</>
 	)
 }
