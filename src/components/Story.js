3aee6c40dd252cbcfabcbd9ffb26593fa19db591
Templars <antoine.ratat@gmail.com>
2021-08-06 15:20:06 +0800

    article story

--- src/components/Story.js
+++ src/components/Story.js
@@ -41,7 +41,7 @@ export default function Story() {
 	return (
 		<>
 			<div ref={home}></div>
-			<Container fluid className='disable-select'>
+			<Container fluid className='disable-select container_story'>
 				<Navbar className='navbar' />
 				<div id='overlay'></div>
 				<Container className='container_home'>
@@ -66,6 +66,7 @@ export default function Story() {
 						<h1 className='title_story'>EXCEPTIONAL QUALITY PROSECCO MADE USING SUSTAINABLE METHODS</h1>
 					</Col>
 				</Row>
+
 				<Animated animationIn='fadeIn' animationOut='fadeOut' animationInDuration={1000} isVisible={true}>
 					<Row className='row_picture_story_1'>
 						<Col className='col_picture_story_1'>
@@ -82,6 +83,27 @@ export default function Story() {
 							<Image
 								className='col_picture_story_picture_1'
 								src='https://www.datocms-assets.com/25196/1597320030-dellavite02b1574.jpg?q=50&auto=format&dpr=1&w=1920&h=1540&fit=crop'
+							/>
+						</Col>
+					</Row>
+				</Animated>
+
+				<Animated animationIn='fadeIn' animationOut='fadeOut' animationInDuration={1000} isVisible={true}>
+					<Row className='row_picture_story_1'>
+						<Col className='col_picture_story_3'>
+							<div>
+								<h1 className='black'>An Untold Story</h1>
+								<p>
+									Beneath the Dolomite mountains, the hills of Valdobbiadene are alive with the local culture, knowledge and passion for
+									making Prosecco. As it's so little known to the world beyond, Della Vite (meaning ‘from the vine’) sets out to tell
+									Prosecco’s real story.{' '}
+								</p>
+							</div>
+						</Col>
+						<Col className='col_picture_story_4'>
+							<Image
+								className='col_picture_story_picture_1'
+								src='https://www.datocms-assets.com/25196/1598382040-c7a0513.jpeg?q=50&auto=format&dpr=1&w=1920&h=1540&fit=crop'
 							/>
 						</Col>
 					</Row>
