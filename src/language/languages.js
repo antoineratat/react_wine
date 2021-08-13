ec1734ba5c9066ca4fc15df4bfbb96154117d01f
Templars <antoine.ratat@gmail.com>
2021-08-13 18:34:52 +0800

    update language french

--- src/language/languages.js
+++ src/language/languages.js
@@ -32,7 +32,7 @@ export const dictionaryList = {
 		footer_story: 'Story',
 		footer_winery: 'Winery',
 		footer_contact: 'Contact',
-		footer_website: '2021 © Wine',
+		footer_website: '2021 © Le Village',
 		footer_website_by: 'Website by Antoine Ratat',
 
 		// NAVBAR
@@ -110,9 +110,9 @@ export const dictionaryList = {
 		home_title: 'SPECIALIST IMPORTER OF WINES FROM FRANCE',
 		home_wine_name_1: 'Vallée du Rhône',
 		home_wine_description_1: 'Red Wine - IGP',
-		home_wine_name_2: 'Muscat Beaumes-de-Venise',
+		home_wine_name_2: 'Muscat',
 		home_wine_description_2: 'White Wine - AOC',
-		home_wine_name_3: 'Muscat Beaumes-de-Venise',
+		home_wine_name_3: 'Sparkling Muscat',
 		home_wine_description_3: 'Sparkling White Wine - AOC',
 
 		// CONTACT
@@ -136,7 +136,7 @@ export const dictionaryList = {
 		footer_story: 'Story',
 		footer_winery: 'Winery',
 		footer_contact: 'Contact',
-		footer_website: '2021 © Wine',
+		footer_website: '2021 © Le Village',
 		footer_website_by: 'Website by Antoine Ratat',
 
 		// NAVBAR
@@ -176,15 +176,15 @@ export const dictionaryList = {
 
 		wine_card_title_1: 'Vallée du Rhône',
 		wine_card_description_1:
-			'a pattern of black fruits and spices. Grown organically and vinified without added sulphites, this good red wine has a nice balance in the mouth.',
+			'A pattern of black fruits and spices. Grown organically and vinified without added sulphites, this good red wine has a nice balance in the mouth.',
 		wine_card_location_1: '44,0709° N, 5,0127° E, BEAUMES-DE-VENISE, FRANCE',
 		wine_card_taste_1: 'Extra Dry',
 		wine_card_acidity_1: '5.5G/L',
 		wine_card_alcohol_1: '13.5%',
 		wine_card_pressure_1: '5.5 ATM',
-		wine_card_grapes_1: '100% GRENACHE',
+		wine_card_grapes_1: '70% GRENACHE 20% GARIGNAN 10% CINSAULT',
 
-		wine_card_title_2: 'Muscat Beaumes-de-Venise',
+		wine_card_title_2: 'Muscat',
 		wine_card_description_2:
 			'The Muscat Beaumes de Venise is produced exclusively from muscat with small grains. This world-renowned white wine is distinguished by its beautiful aromatic persistence and freshness.',
 		wine_card_location_2: '44,0709° N, 5,0127° E, BEAUMES-DE-VENISE, FRANCE',
@@ -194,7 +194,7 @@ export const dictionaryList = {
 		wine_card_pressure_2: '5.5 ATM',
 		wine_card_grapes_2: '100% MUSCAT',
 
-		wine_card_title_3: 'Muscat Beaumes-de-Venise',
+		wine_card_title_3: 'Sparkling Muscat',
 		wine_card_description_3:
 			'The Muscat Beaumes de Venise seduces the eye with its tiled pink dress. The fruit is fair in this sweet wine, with aromas of candied citrus fruits and orange blossom.',
 		wine_card_location_3: '44,0709° N, 5,0127° E, BEAUMES-DE-VENISE, FRANCE',
@@ -205,23 +205,23 @@ export const dictionaryList = {
 		wine_card_grapes_3: '100% MUSCAT',
 	},
 	'fr-FR': {
-		learn_more: 'Learn More',
+		learn_more: 'En savoir plus',
 
 		// HOME
 		home_title: 'SPECIALIST IMPORTER OF WINES FROM FRANCE',
 		home_wine_name_1: 'Vallée du Rhône',
-		home_wine_description_1: 'Red Wine - IGP',
-		home_wine_name_2: 'Muscat Beaumes-de-Venise',
-		home_wine_description_2: 'White Wine - AOC',
-		home_wine_name_3: 'Muscat Beaumes-de-Venise',
-		home_wine_description_3: 'Sparkling White Wine - AOC',
+		home_wine_description_1: 'Vin Rouge - IGP',
+		home_wine_name_2: 'Muscat',
+		home_wine_description_2: 'Vin Blanc - AOC',
+		home_wine_name_3: 'Sparkling Muscat',
+		home_wine_description_3: 'Vin Blanc Pétillant - AOC',
 
 		// CONTACT
 		contact_company_name: 'Le Village Distribution',
 		contact_company_description:
 			'Le Village Distribution represents five wineries from three different regions of France that have a high vocation in viticulture. Le Village Distribution currently produces 15 different varieties of fine wines. Each vineyard is managed with passion and respect for its tradition, in accordance with its territory of origin.',
 		contact_company_location: '30.40001° N 104.04001° E, CHENGDU, CHINA',
-		contact_label_phone: 'Phone',
+		contact_label_phone: 'Téléphone',
 		contact_phone: '13111881660',
 		contact_phone_cn: '18008008656',
 		contact_label_email: 'Email',
@@ -233,73 +233,73 @@ export const dictionaryList = {
 		contact_skype: '@18008008656',
 
 		// FOOTER
-		footer_wines: 'Wines',
-		footer_story: 'Story',
-		footer_winery: 'Winery',
+		footer_wines: 'Vins',
+		footer_story: 'Histoire',
+		footer_winery: 'Vignobles',
 		footer_contact: 'Contact',
-		footer_website: '2021 © Wine',
-		footer_website_by: 'Website by Antoine Ratat',
+		footer_website: '2021 © Le Village',
+		footer_website_by: 'Antoine Ratat',
 
 		// NAVBAR
 		navbar_brand: 'Le Village',
-		navbar_wines: 'Wines',
-		navbar_story: 'Story',
-		navbar_winery: 'Winery',
+		navbar_wines: 'Vins',
+		navbar_story: 'Histoire',
+		navbar_winery: 'Vignobles',
 		navbar_contact: 'Contact',
 
 		// STORY
-		story_title: 'OUR JOURNEY OF DISCOVERY',
-		story_subtitle: 'EXCEPTIONAL QUALITY WINE MADE USING SUSTAINABLE METHODS',
-		story_card_title_1: 'Connect Cultures',
+		story_title: 'NOTRE VOYAGE DE DECOUVERTE',
+		story_subtitle: 'UN VIN DE QUALITÉ EXCEPTIONNEL ÉLABORÉ AVEC DES MÉTHODES DURABLES',
+		story_card_title_1: 'Connecte les cultures',
 		story_card_description_1:
-			'Le Village is growing and remains fully committed to its original purpose: reaching out and connect cultures to discerning wine lovers from all over China.',
-		story_card_title_2: 'AN INSPIRATIONAL LANDSCAPE',
+			'Le Village grandit et reste pleinement attaché à son objectif initial : atteindre et connecter les cultures aux amateurs de vin exigeants de toute la Chine.',
+		story_card_title_2: 'UN PAYSAGE INSPIRANT',
 		story_card_description_2:
-			'A region of quiet valleys and dramatic skylines, grapes have been grown here since ancient times. At the heart of artisanal French wine culture, selected for conservation for its cultural and environmental significance.',
+			"Une région de vallées tranquilles et d'horizons spectaculaires, les raisins sont cultivés ici depuis l'Antiquité. Au cœur de la culture artisanale du vin français, sélectionné pour la conservation pour son importance culturelle et environnementale.",
 
 		// WINERY
-		winery_title: 'EXPLORE OUR WINERIES',
-		winery_subtitle: 'We have an incredibly unique and diverse portfolio of wineries',
+		winery_title: 'EXPLOREZ NOS VIGNOBLES',
+		winery_subtitle: 'Nous avons un portefeuille incroyablement unique et diversifié de vignobles',
 		winery_card_title_1: 'LES BACCHANALES',
 		winery_card_description_1:
-			'It is the great diversity of soil along with the cool evenings and sun-drenched days that create ideal growing conditions for Muscat varietals. At Les Bacchanales, we are dedicated to growing Muscat and Sparkling with the explicit intention of producing the most authentic expression possible.',
+			"C'est la grande diversité des sols ainsi que les soirées fraîches et les journées ensoleillées qui créent des conditions de croissance idéales pour les cépages Muscat. Chez Les Bacchanales, nous consacrons à la culture du Muscat et du Mousseux avec l'intention explicite de produire l'expression la plus authentique possible.",
 		winery_card_title_2: 'MALTOFF',
 		winery_card_description_2:
-			'This winery specializes in producing the red wines Burgundy is famous for like Chardonnay,  Pinot Noir etc. And there are also wine-tasting events organized here that consists of red, white and rosé wines.',
+			'Cette cave est spécialisée dans la production des vins rouges pour lesquels la Bourgogne est célèbre comme le Chardonnay, le Pinot Noir, etc. Et il y a aussi des événements de dégustation de vins organisés ici qui se composent de vins rouges, blancs et rosés.',
 
 		// WINES
 
-		wine_label_taste: 'Taste',
-		wine_label_acidity: 'Acidity',
-		wine_label_alcohol: 'Alcohol',
-		wine_label_pressure: 'Pressure',
-		wine_label_grape: 'Grapes',
+		wine_label_taste: 'Goût',
+		wine_label_acidity: 'Acidité',
+		wine_label_alcohol: 'Alcool',
+		wine_label_pressure: 'Pression',
+		wine_label_grape: 'Raisins',
 
 		wine_card_title_1: 'Vallée du Rhône',
 		wine_card_description_1:
-			'a pattern of black fruits and spices. Grown organically and vinified without added sulphites, this good red wine has a nice balance in the mouth.',
+			"Un motif de fruits noirs et d'épices. Issu de l'agriculture biologique et vinifié sans sulfites ajoutés, ce bon vin rouge possède un bel équilibre en bouche.",
 		wine_card_location_1: '44,0709° N, 5,0127° E, BEAUMES-DE-VENISE, FRANCE',
-		wine_card_taste_1: 'Extra Dry',
+		wine_card_taste_1: 'Sec',
 		wine_card_acidity_1: '5.5G/L',
 		wine_card_alcohol_1: '13.5%',
 		wine_card_pressure_1: '5.5 ATM',
-		wine_card_grapes_1: '100% GRENACHE',
+		wine_card_grapes_1: '70% GRENACHE 20% GARIGNAN 10% CINSAULT',
 
-		wine_card_title_2: 'Muscat Beaumes-de-Venise',
+		wine_card_title_2: 'Muscat',
 		wine_card_description_2:
-			'The Muscat Beaumes de Venise is produced exclusively from muscat with small grains. This world-renowned white wine is distinguished by its beautiful aromatic persistence and freshness.',
+			'Le Muscat Beaumes de Venise est produit exclusivement à partir de muscat à petits grains. Ce vin blanc de renommée mondiale se distingue par sa belle persistance aromatique et sa fraîcheur.',
 		wine_card_location_2: '44,0709° N, 5,0127° E, BEAUMES-DE-VENISE, FRANCE',
-		wine_card_taste_2: 'Sweet',
+		wine_card_taste_2: 'Sucré',
 		wine_card_acidity_2: '4.5G/L',
 		wine_card_alcohol_2: '13.5%',
 		wine_card_pressure_2: '5.5 ATM',
 		wine_card_grapes_2: '100% MUSCAT',
 
-		wine_card_title_3: 'Muscat Beaumes-de-Venise',
+		wine_card_title_3: 'Muscat Pétillant',
 		wine_card_description_3:
 			'The Muscat Beaumes de Venise seduces the eye with its tiled pink dress. The fruit is fair in this sweet wine, with aromas of candied citrus fruits and orange blossom.',
 		wine_card_location_3: '44,0709° N, 5,0127° E, BEAUMES-DE-VENISE, FRANCE',
-		wine_card_taste_3: 'Sweet Sparkling',
+		wine_card_taste_3: 'Sucré pétillant',
 		wine_card_acidity_3: '4.5G/L',
 		wine_card_alcohol_3: '13.5%',
 		wine_card_pressure_3: '5.5 ATM',
