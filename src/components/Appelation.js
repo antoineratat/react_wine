cc523b36a654745dd3cb1a2775359db14acc730e
Antoine Ratat <antoine.ratat@gmail.com>
2021-11-26 03:38:44 +0800

    responsive appelations

--- src/components/Appelation.js
+++ src/components/Appelation.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Row, Col, Image } from 'react-bootstrap'
-import { Animated } from 'react-animated-css'
 
 export default function Appelation(props) {
 	const { appelName, appelDesc, img } = props
 
 	return (
 		<Row className='row_appel_1'>
-			<Col className='col_appel_image' xs={12} sm={12} md={4} lg={4} xl={3}>
+			<Col className='col_appel_image' xs={{ span: 12, order: 2 }} lg={4} xl={3}>
 				<Image className='image_appel_1' src={img} />
 			</Col>
-			<Col className='col_appel_desc' xs={12} md={12} lg={8} xl={9}>
+			<Col className='col_appel_desc' xs={{ span: 12, order: 1 }} lg={8} xl={9}>
 				<Row>
 					<Col className='appel_right'>
 						<Row>
