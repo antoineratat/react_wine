9a070cc7144a3c02abd407edbfb9d514dc10e983
Antoine Ratat <antoine.ratat@gmail.com>
2021-11-26 03:40:19 +0800

    change order mobile

--- src/components/Appelation.js
+++ src/components/Appelation.js
@@ -6,10 +6,18 @@ export default function Appelation(props) {
 
 	return (
 		<Row className='row_appel_1'>
-			<Col className='col_appel_image' xs={{ span: 12, order: 2 }} lg={4} xl={3}>
+			<Col
+				className='col_appel_image'
+				xs={{ span: 12, order: 2 }}
+				lg={{ span: 4, order: 1 }}
+				xl={{ span: 3, order: 1 }}>
 				<Image className='image_appel_1' src={img} />
 			</Col>
-			<Col className='col_appel_desc' xs={{ span: 12, order: 1 }} lg={8} xl={9}>
+			<Col
+				className='col_appel_desc'
+				xs={{ span: 12, order: 1 }}
+				lg={{ span: 8, order: 2 }}
+				xl={{ span: 9, order: 2 }}>
 				<Row>
 					<Col className='appel_right'>
 						<Row>
