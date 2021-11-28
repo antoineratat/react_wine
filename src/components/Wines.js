d655caa583c2f891c4f4cf7b291d10eac5730bfd
Antoine Ratat <antoine.ratat@gmail.com>
2021-11-28 16:08:17 +0800

    wine mirror card

--- src/components/Wines.js
+++ src/components/Wines.js
@@ -27,17 +27,17 @@ export default function Wines(props) {
 		)
 		observerRow1.observe(row_wines_1.current)
 
-		// let observerRow2 = new IntersectionObserver(
-		// 	(entries) =>
-		// 		entries.forEach((entry) => {
-		// 			if (entry.isIntersecting) {
-		// 				setVisibleRow2(true)
-		// 				observerRow2 = observerRow2.disconnect()
-		// 			}
-		// 		}),
-		// 	{ rootMargin: '0px 0px -400px 0px' }
-		// )
-		// observerRow2.observe(row_wines_2.current)
+		let observerRow2 = new IntersectionObserver(
+			(entries) =>
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						setVisibleRow2(true)
+						observerRow2 = observerRow2.disconnect()
+					}
+				}),
+			{ rootMargin: '0px 0px -400px 0px' }
+		)
+		observerRow2.observe(row_wines_2.current)
 
 		// let observerRow3 = new IntersectionObserver(
 		// 	(entries) =>
@@ -58,6 +58,7 @@ export default function Wines(props) {
 				<Navbar className='navbar' handleUpdateLanguage={props.handleUpdateLanguage} language={language} />
 				<div id='overlay'></div>
 				<Container fluid className='container_wines'>
+					{/* WINE1 */}
 					<Row ref={row_wines_1} className='row_wines'>
 						<Col xs={{ span: 12 }} md={{ span: 4 }} lg={{ span: 3 }} xl={{ span: 3 }}>
 							<Animated
@@ -93,50 +94,13 @@ export default function Wines(props) {
 						</Col>
 					</Row>
 
-					{/* ROW 2 */}
-					{/* <Row className='row_wines_1' ref={row_wines_2}>
-						<Col className='col_wines_3'>
-							<Animated
-								className='animated_wines'
-								animationIn='fadeIn'
-								animationOut='fadeOut'
-								animationInDuration={2000}
-								animationOutDuration={0}
-								isVisible={visibleRow2}>
-								<Row className='wine_description'>
-									<Col className='wine_description_left'>
-										<h1 style={language === 'zh-CN' ? { fontFamily: 'hanzhen' } : {}}>
-											{dictionary.wine_card_title_2}
-										</h1>
-										<h4>{dictionary.wine_card_description_2}</h4>
-										<p>{dictionary.wine_card_location_2}</p>
-									</Col>
-									<Col className='wine_description_right'>
-										<Row>
-											<p className='title_contact'>{dictionary.wine_label_taste}</p>
-											<p className='text_contact'>{dictionary.wine_card_taste_2}</p>
-										</Row>
-										<Row>
-											<p className='title_contact'>{dictionary.wine_label_acidity}</p>
-											<p className='text_contact'>{dictionary.wine_card_acidity_2}</p>
-										</Row>
-										<Row>
-											<p className='title_contact'>{dictionary.wine_label_alcohol}</p>
-											<p className='text_contact'>{dictionary.wine_card_alcohol_2}</p>
-										</Row>
-										<Row>
-											<p className='title_contact'>{dictionary.wine_label_pressure}</p>
-											<p className='text_contact'>{dictionary.wine_card_pressure_2}</p>
-										</Row>
-										<Row>
-											<p className='title_contact'>{dictionary.wine_label_grape}</p>
-											<p className='text_contact'>{dictionary.wine_card_grapes_2}</p>
-										</Row>
-									</Col>
-								</Row>
-							</Animated>
-						</Col>
-						<Col className='col_wines_4'>
+					{/* WINE2 */}
+					<Row ref={row_wines_2} className='row_wines'>
+						<Col
+							xs={{ order: 1, span: 12 }}
+							md={{ order: 2, span: 4 }}
+							lg={{ order: 2, span: 3 }}
+							xl={{ order: 2, span: 3 }}>
 							<Animated
 								className='animated_wines'
 								animationIn='fadeInUp'
@@ -150,7 +114,29 @@ export default function Wines(props) {
 								/>
 							</Animated>
 						</Col>
-					</Row> */}
+						<Col
+							xs={{ order: 2, span: 12 }}
+							md={{ order: 1, span: 8 }}
+							lg={{ order: 1, span: 9 }}
+							xl={{ order: 1, span: 9 }}>
+							<Animated
+								className='animated_wines'
+								animationIn='fadeIn'
+								animationOut='fadeOut'
+								animationInDuration={2000}
+								animationOutDuration={0}
+								isVisible={visibleRow2}>
+								<Row className='wine_description'>
+									<Col className='wine_description_left'>
+										<h1 style={language === 'zh-CN' ? { fontFamily: 'hanzhen' } : {}}>
+											{dictionary.wine_card_title_1}
+										</h1>
+										<h4>{dictionary.wine_card_description_1}</h4>
+									</Col>
+								</Row>
+							</Animated>
+						</Col>
+					</Row>
 				</Container>
 				<Footer />
 			</Container>
