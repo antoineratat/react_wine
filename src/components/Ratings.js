ad04830092376a6e26124596355791b07e027f70
Antoine Ratat <antoine.ratat@gmail.com>
2021-12-02 01:04:00 +0800

    update rating

--- src/components/Ratings.js
+++ src/components/Ratings.js
@@ -1,5 +1,23 @@
 import React from 'react'
+import { Row, Col } from 'react-bootstrap'
 
 export default function Ratings() {
-	return <div className='ratings'>Ratings: Vivino (Link) / Robert Parker (Link)</div>
+	return (
+		<Row className='ratings'>
+			<Col>
+				<div>Ratings: Vivino (Link) / Robert Parker (Link)</div>
+			</Col>
+			<Col>
+				<input
+					class='rating'
+					max='5'
+					readonly
+					step='0.01'
+					style={{ '--value': '4.3' }}
+					type='range'
+					value='4.3'
+				/>
+			</Col>
+		</Row>
+	)
 }
