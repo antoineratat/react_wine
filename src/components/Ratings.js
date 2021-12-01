7245fc7c9c6b680da66f3b9357e53e3fe944b159
Antoine Ratat <antoine.ratat@gmail.com>
2021-12-02 00:15:56 +0800

    update design ratings

--- src/components/Ratings.js
+++ src/components/Ratings.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Ratings() {
-	return <div style={{ color: 'white' }}>Ratings: Vivino (Link) / Robert Parker (Link)</div>
+	return <div className='ratings'>Ratings: Vivino (Link) / Robert Parker (Link)</div>
 }
